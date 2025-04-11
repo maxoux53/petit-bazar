af9be1c6328e44d37a78ec16a6aa58a7d93604f6
Max <71935156+maxoux53@users.noreply.github.com>
2025-04-11 12:33:19 +0200

    edit method returns updated lines

--- src/dataAccess/IEmployeeDAO.java
+++ src/dataAccess/IEmployeeDAO.java
@@ -4,10 +4,10 @@ import model.Employee;
 import java.util.ArrayList;
 
 public interface IEmployeeDAO {
-    void create(Employee employee) throws InsertionFailedException, DAORetrievalFailedException; // void? boolean? Employee? with exception?
+    int create(Employee employee) throws InsertionFailedException, DAORetrievalFailedException; // void? boolean? Employee? with exception?
     void deleteById(int id) throws DeleteFailedException, DAORetrievalFailedException;
     Employee findById(int id) throws NotFoundException, DAORetrievalFailedException;
     ArrayList<Employee> findAll() throws DAORetrievalFailedException;
-    void edit(Employee employee) throws UpdateFailedException;
+    int edit(Employee employee) throws UpdateFailedException, DAORetrievalFailedException;
 
 }
