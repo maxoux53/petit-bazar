5d7d89e671293de6b51b1ef092120a400c434725
Max <71935156+maxoux53@users.noreply.github.com>
2025-04-11 18:46:47 +0200

    product findAll method

--- src/dataAccess/IProductDAO.java
+++ src/dataAccess/IProductDAO.java
@@ -7,7 +7,7 @@ public interface IProductDAO {
     void create(Product product) throws InsertionFailedException;
     void delete(Product product) throws DeleteFailedException;
     Product findByBarcode(int barcode) throws NotFoundException, DAORetrievalFailedException;
-    ArrayList<Product> findAll();
+    ArrayList<Product> findAll() throws DAORetrievalFailedException;
     void edit(Product product) throws UpdateFailedException;
 
 }
--- src/dataAccess/ProductDBAccess.java
+++ src/dataAccess/ProductDBAccess.java
@@ -18,10 +18,11 @@ public class ProductDBAccess implements IProductDAO {
     }
 
     public Product findByBarcode(int barcode) throws NotFoundException, DAORetrievalFailedException {
-        sqlInstruction = "SELECT * FROM product WHERE barcode = " + barcode + ";";
+        sqlInstruction = "SELECT * FROM product WHERE barcode = ?;";
 
         try {
             preparedStatement = SingletonConnection.getInstance().prepareStatement(sqlInstruction);
+            preparedStatement.setInt(1, barcode);
 
             data = preparedStatement.executeQuery();
 
@@ -88,8 +89,78 @@ public class ProductDBAccess implements IProductDAO {
         }
     }
 
-    public ArrayList<Product> findAll() {
-        return null;
+    public ArrayList<Product> findAll() throws DAORetrievalFailedException {
+        sqlInstruction = "SELECT * FROM product;";
+
+        try {
+            preparedStatement = SingletonConnection.getInstance().prepareStatement(sqlInstruction);
+            preparedStatement.executeQuery();
+
+            ArrayList<Product> products = new ArrayList<>();
+
+            Product product;
+            int barcode;
+            String name;
+            String description;
+            int amount;
+            boolean isAvailable;
+            char vatType;
+            int categoryId;
+            int brandId;
+            int supplierVatNumber;
+
+            while (data.next()) {
+                product = new Product(data.getInt("barcode")); // what if barcode is null?
+
+                name = data.getString("name");
+                if (!data.wasNull()) {
+                    product.setName(name);
+                }
+
+                description = data.getString("description");
+                if (!data.wasNull()) {
+                    product.setDescription(description);
+                }
+
+                amount = data.getInt("amount");
+                if (!data.wasNull()) {
+                    product.setAmount(amount);
+                }
+
+                isAvailable = data.getBoolean("is_available");
+                if (!data.wasNull()) {
+                    product.setAvailable(isAvailable);
+                }
+
+                vatType = data.getString("vat_type").charAt(0);
+                if (!data.wasNull()) {
+                    product.setVatType(vatType);
+                }
+
+                categoryId = data.getInt("category_id");
+                if (!data.wasNull()) {
+                    product.setCategoryId(categoryId);
+                }
+
+                brandId = data.getInt("brand_id");
+                if (!data.wasNull()) {
+                    product.setBrandId(brandId);
+                }
+
+                supplierVatNumber = data.getInt("supplier_vat_number");
+                if (!data.wasNull()) {
+                    product.setSupplierVatNumber(supplierVatNumber);
+                }
+
+                products.add(product);
+            }
+
+            return products;
+        } catch (SQLTimeoutException e) {
+            throw new DAORetrievalFailedException("Database query timed out!", e.getMessage());
+        } catch (SQLException e) {
+            throw new DAORetrievalFailedException("SQL data access failed!", e.getMessage());
+        }
     }
 
     public void edit(Product product) throws UpdateFailedException {
