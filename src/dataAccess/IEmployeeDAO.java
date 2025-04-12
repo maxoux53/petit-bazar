91b380f7b8cd006e9cb745f1421fd09408c7de5b
Max <71935156+maxoux53@users.noreply.github.com>
2025-04-12 21:34:01 +0200

    update employee + reorder methods

--- src/dataAccess/IEmployeeDAO.java
+++ src/dataAccess/IEmployeeDAO.java
@@ -6,8 +6,9 @@ import java.util.ArrayList;
 public interface IEmployeeDAO {
     int create(Employee employee) throws InsertionFailedException, DAORetrievalFailedException; // void? boolean? Employee? with exception?
     void deleteById(int id) throws DeleteFailedException, DAORetrievalFailedException;
+    int edit(Employee employee) throws UpdateFailedException, DAORetrievalFailedException;
+
     Employee findById(int id) throws NotFoundException, DAORetrievalFailedException;
     ArrayList<Employee> findAll() throws DAORetrievalFailedException;
-    int edit(Employee employee) throws UpdateFailedException, DAORetrievalFailedException;
 
 }
