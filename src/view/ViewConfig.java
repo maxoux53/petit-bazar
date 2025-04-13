8d4ae7ef60cf8782a0f1fd60ba2dbeca2143ff62
Max <71935156+maxoux53@users.noreply.github.com>
2025-04-13 11:52:37 +0200

    globalize dotenv using singleton

---
+++ src/view/ViewConfig.java
@@ -0,0 +1,7 @@
+package view;
+
+import Utils.Env;
+
+public class ViewConfig { // PEUT POTENTIELLEMENT ÊTRE INTEGRÉE DANS UNE AUTRE CLASSE DE VUE
+    public static final String NAME = Env.getDotenv().get("STORE_NAME");
+}
