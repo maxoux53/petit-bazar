bb4cc8d3e4e6f9a57551f8a437f1bc0a14109e40
Max <71935156+maxoux53@users.noreply.github.com>
2025-04-14 22:58:02 +0200

    Create EmployeeManager.java

---
+++ src/business/EmployeeManager.java
@@ -0,0 +1,54 @@
+package business;
+
+import dataAccess.*;
+import model.Employee;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+
+public class EmployeeManager {                           // static?
+    private static IEmployeeDAO dao;
+    private static MessageDigest digest;
+
+    static {
+        dao = new EmployeeDBAccess();
+    }
+
+    public static void add(Employee employee) throws InsertionFailedException, DAORetrievalFailedException {
+        dao.create(employee);
+    }
+
+    public static void remove(int id) throws DeleteFailedException, DAORetrievalFailedException {
+        dao.deleteById(id);                                          // todo: handle returned int
+    }
+
+    public static Employee getById(int id) throws NotFoundException, DAORetrievalFailedException {
+        return dao.findById(id);
+    }
+
+    public static ArrayList<Employee> getAll() throws DAORetrievalFailedException {
+        return dao.findAll();
+    }
+
+    public static void update(Employee employee) throws DAORetrievalFailedException, UpdateFailedException {
+        dao.edit(employee);
+    }
+
+    public static byte[] hashPassword(String password) throws HashFailedException {
+        if (digest == null) {
+            try {
+                digest = MessageDigest.getInstance("SHA-256");
+            } catch (Exception e) {
+                throw new HashFailedException(e.getMessage());
+            }
+        }
+
+        return digest.digest(password.getBytes());
+    }
+
+    public static boolean checkPassword(String passwordAttempt, int id) throws HashFailedException {
+        return MessageDigest.isEqual(
+                hashPassword(passwordAttempt),
+                dao.getPasswordHash(id) // todo: create method in data access layer
+        );
+    }
+}
