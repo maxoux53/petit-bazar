b92992f52bea7c3d9390d135dce484133e360fcb
Max <71935156+maxoux53@users.noreply.github.com>
2025-04-26 17:28:15 +0200

    create entry point structure

--- src/view/ViewConfig.java
+++
@@ -1,7 +0,0 @@
-package view;
-
-import utils.Env;
-
-public class ViewConfig { // PEUT POTENTIELLEMENT ÊTRE INTEGRÉE DANS UNE AUTRE CLASSE DE VUE
-    public static final String NAME = Env.getDotenv().get("STORE_NAME");
-}
