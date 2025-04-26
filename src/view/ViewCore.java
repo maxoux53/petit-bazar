9953112403861341d2d282670a4ee9eb15fcad81
Max <71935156+maxoux53@users.noreply.github.com>
2025-04-26 17:28:15 +0200

    create entry point structure

---
+++ src/view/ViewCore.java
@@ -0,0 +1,39 @@
+package view;
+
+import Utils.Env;
+import business.DataInteractionManager;
+import dataAccess.DAORetrievalFailedException;
+
+import javax.swing.*;
+
+public class ViewCore {
+    public static final String NAME;
+    private static Window window;
+
+    static {
+        NAME = Env.getDotenv().get("STORE_NAME");
+    }
+
+    public static void init() {
+        applyPlatformSpecificSettings();
+        window = new Window(NAME);
+    }
+
+    public static void quit() {
+        try {
+            DataInteractionManager.terminate();
+            System.exit(0);
+        } catch (DAORetrievalFailedException e) {
+            JOptionPane.showMessageDialog(null, "Une erreur est survenue lors de la fermeture ! Veuillez reessayer.\n" + e.getMessage(), "⛔️", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private static void applyPlatformSpecificSettings() {
+        // Apple macOS
+        if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+            System.setProperty("apple.awt.application.name", NAME);
+            System.setProperty("apple.awt.application.appearance", "system");
+        }
+    }
+}
