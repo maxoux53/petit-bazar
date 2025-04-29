817945033625d036e62e08f957c7372d470da1c6
Max <71935156+maxoux53@users.noreply.github.com>
2025-04-30 00:01:05 +0200

    Create EmployeeListing.java

---
+++ src/view/EmployeeListing.java
@@ -0,0 +1,46 @@
+package view;
+
+import controller.EmployeeController;
+import dataAccess.DAORetrievalFailedException;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class EmployeeListing extends JPanel {
+    // Attributes
+    JPanel titlePanel, listingPanel;
+    JLabel titleLabel;
+    JTable listingTable;
+    JScrollPane listingScroll;
+
+    // Constructor
+    public EmployeeListing() {
+        setLayout(new BorderLayout(0, 100));
+        setBorder(BorderFactory.createEmptyBorder(20, 100, 100, 100));
+        new GridLayout(2, 1, 0, 0);
+        setBackground(Color.white);
+
+        // Titles
+        titlePanel = new JPanel();
+        titlePanel.setBackground(Color.white);
+
+        titleLabel = new JLabel("Liste des employés", SwingConstants.CENTER);
+        titleLabel.setFont(new Font(FontPreferences.DEFAULT_STYLE.getStyle(), Font.BOLD, FontPreferences.TITLE_SIZE.getSize()));
+
+        titlePanel.add(titleLabel);
+
+        // Listing
+        listingPanel = new JPanel();
+        listingPanel.setBackground(Color.red);
+
+        try {
+            listingTable = new JTable(EmployeeController.infoTableModel());
+
+            // Scroll
+            listingScroll = new JScrollPane(listingTable);
+            add(listingScroll, BorderLayout.CENTER);
+        } catch (DAORetrievalFailedException e) {
+            JOptionPane.showMessageDialog(null, "Erreur de récupération des données", "Erreur", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
