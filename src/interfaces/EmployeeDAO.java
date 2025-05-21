a449369b6b30bd23cd09d5be680f0f02d8fe5aab
Zencaw <161458851+Zencaaw@users.noreply.github.com>
2025-05-21 16:23:43 +0200

    fixe setCity

--- src/interfaces/EmployeeDAO.java
+++ src/interfaces/EmployeeDAO.java
@@ -8,11 +8,11 @@ import java.util.ArrayList;
 
 public interface EmployeeDAO {
     int create(Employee employee) throws InsertionFailedException, DAORetrievalFailedException;
-    int remove(int id) throws DeleteFailedException, DAORetrievalFailedException;
+    int remove(Integer id) throws DeleteFailedException, DAORetrievalFailedException;
     int update(Employee employee) throws UpdateFailedException, DAORetrievalFailedException;
-    Employee getById(int id) throws NotFoundException, DAORetrievalFailedException, ProhibitedValueException;
+    Employee getById(Integer id) throws NotFoundException, DAORetrievalFailedException, ProhibitedValueException;
     ArrayList<Employee> getAll() throws DAORetrievalFailedException, ProhibitedValueException;
-    byte[] getPasswordHash(int id) throws NotFoundException, DAORetrievalFailedException;
+    byte[] getPasswordHash(Integer id) throws NotFoundException, DAORetrievalFailedException;
     ArrayList<String> getAllCountries() throws DAORetrievalFailedException;
     ArrayList<String> getAllRoles() throws DAORetrievalFailedException;
 }
